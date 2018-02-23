add633fa25b227a41b47b67b50882925d739a25e
Heinrich van Vught <heinrich.van.vught@gmail.com>
2018-02-23 13:06:15 +0000

    Reconfigured osx virtual environment and changed to using pytest

--- test_SampleFunctions.py
+++ test_SampleFunctions.py
@@ -1,7 +1,30 @@
 from unittest import TestCase
 from SampleFunctions import add
-from hypothesis import given, example
+from hypothesis import given, example, assume
 import hypothesis.strategies as st
+
+
+# example how to limit test input using assume
+@given(st.integers(), st.integers())
+def test_my_test_filtering_using_assume(a, b):
+    assume(a > -1)
+    print(f"Running test_my_test_filtering_using_assume({a}, {b})")
+    assert(1 == 1)
+
+
+# example how to limit test input using assume
+@given(st.integers(0, 1000), st.integers(-1000, 0))
+def test_my_test_filtering_using_strategy_args(a, b):
+    print(f"Running test_my_test_filtering_using_strategy_args({a}, {b})")
+    assert(1 == 1)
+
+
+# example how to limit test input using assume
+@given(st.integers().filter(lambda x: 0 < x < 1001), st.integers())
+def test_my_test_filtering_using_lambda_filters(a, b):
+    assume(a > -1)
+    print(f"Running test_my_test_filtering_using_lambda_filters({a}, {b})")
+    assert(1 == 1)
 
 
 class TestSampleFunctions(TestCase):
@@ -20,3 +43,31 @@ class TestSampleFunctions(TestCase):
             assert(e == actual)
         else:
             assert((a+b) == actual)
+
+    # @given(a=st.integers(), b=st.integers(), c=st.just(30))
+    # # @given(a=st.just(10), b=st.just(20), c=st.just(30))
+    # #  @example(a=10, b=20, c=30)
+    # def test_my_py_test(self, a, b, c):
+    #     # a = 10
+    #     # b = 20
+    #     # c = 30
+    #     print(f"Running test_my_py_test({a}, {b}, {c})")
+    #     actual = add(a, b)
+    #     assert(c == actual)
+
+
+# example to show how to limit a test to a specific set of inputs
+myValue = [10, 10]
+myOtherValue = [10, 30]
+myAnswers = [20, 40]
+
+
+@given(st.just(myValue), st.just(myOtherValue), st.just(myAnswers))
+def test_my_test2(a, b, c):
+    print(f"Running test_my_test2({a}, {b}, {c})")
+    for idx in range(len(myValue)):
+        a1 = a[idx]
+        b1 = b[idx]
+        c1 = c[idx]
+        print(f"Running subtest of test_my_test2({a1}, {b1}, {c1})")
+        assert(a1 + b1 == c1)
